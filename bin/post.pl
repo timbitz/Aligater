18ced9a27e254f6fcdde03c2d5a1853d7aab3bdc
tim <tim.sterne.weiler@utoronto.ca>
2014-10-31 16:59:26 -0400

    added diagonal checker, not debugged

--- bin/post.pl
+++ bin/post.pl
@@ -28,9 +28,12 @@ $path =~ s/\/$0$//;
 my $tmpPath = "$path/../tmp";
 my $libPath = "$path/../lib";
 
+my %toFilter;  #main memory use of the program
+
 # GLOBAL INIT
 my $STRICT = 1;
-my $RUNBLAST = 1;
+my $RUNBLAST = 0;
+my $RUNRACTIP = 0;
 
 my $blastDb = "human_genomic,other_genomic,nt";
 
@@ -39,7 +42,11 @@ my $gcLimit = 0.8;
 
 my $threads = 1;
 
-GetOptions("o=s" => \$outputCore);
+GetOptions("gc=f" => \$gcLimit, 
+           "mono=i" => \$bpMonoLimit,
+           "p=i" => \$threads, 
+           "ractip" => \$RUNRACTIP,
+           "blast" => \$RUNBLAST);
 
 # don't die, explode!
 sub explode {
@@ -85,15 +92,23 @@ while(my $l = <>) {
   chomp($l);
   my(@a) = split(/\t/, $l);
 
+  my $seq = $a[8]; 
+  my $gcContent = gcContent($seq);
+
   # HARD FILTERS ----------------------#
   # mononucleotide tract filter
   next if($seq =~ /[Aa]{$bpMonoLimit}|[Tt]{$bpMonoLimit}|[Cc]{$bpMonoLimit}|[Gg]{$bpMonoLimit}/);
   next if length($seq) < 44; # need at least 22bp on either side.
-  next if gcContent($seq) >= $gcLimit; # greater than limit of gc content
+  next if $gcContent >= $gcLimit; # greater than limit of gc content
   #------------------------------------#
 
-  my($dG, $strA, $strB, $len) = runRactIP($seqA, $seqB, $libPath);
-  print "$dG\t$strA\t$strB\t$len\n";
+  my($seqA, $seqB) = split(/\_/, $seq); 
+
+  my($dG, $strA, $strB, $len, $amt) = runRactIP($seqA, $seqB, "$libPath/rna_andronescu2007.par");
+  my($dG_ua, $strA_ua, $strB_ua, $len_ua) = runRactIP($seqA, $seqB, "$libPath/rna_andronescu2007_ua.par");
+  my $altStruc = ($strA eq $strA_ua and $strB eq $strB_ua) ? "no" : "yes";
+  my $altDG = (abs($len - $len_ua) <= 1) ? $dG_ua - $dG : 0;
+  print ">>$dG\n$strA\t$strB\n$seqA\t$seqB\n$len\t$amt\t$altDG>>>>\n";
  
 } # end main loop
 close FORBLAST;
@@ -111,7 +126,7 @@ if($RUNBLAST) { # lets run blast and remove ligations that aren't unique.
 
 #######################################################
 #                                                     #
-################# BEGIN SUBROUTINES ###################
+######            BEGIN SUBROUTINES            ########
 #                                                     #
 #######################################################
 
@@ -126,22 +141,60 @@ sub runRactIP {
   $seqA =~ s/T/U/g if($seqA =~ /T/);
   $seqB =~ s/T/U/g if($seqB =~ /T/);
   my $rand = substr(md5_hex(rand), 0, 4);   
-  system("echo \">seqA\n$seqA\" > $tmpPath/$rand.seqA.fa"); 
-  system("echo \">seqB\n$seqB\" > $tmpPath/$rand.seqB.fa");
+  system("echo \">seqA\n$seqA\" > $tmpPath/$rand\_seqA.fa"); 
+  system("echo \">seqB\n$seqB\" > $tmpPath/$rand\_seqB.fa");
   $param = defined($param) ? "-P $param" : "";
-  my(@res) = `ractip $tmpPath/$rand.seqA.fa $tmpPath/$rand.seqB.fa -e $param`;
-  my undef = `rm $tmpPath/$rand*`;
+  my(@res) = `ractip $tmpPath/$rand\_seqA.fa $tmpPath/$rand\_seqB.fa -e $param`;
+  system("rm $tmpPath/$rand*");
   chomp @res;
-  my($structA, $structB) = $res[qw(1 3)]; #set structures
+  my($structA, $structB) = ($res[2], $res[5]); #set structures
   $res[6] =~ /JS\= ([\d\-\.]+)/;
   my $deltaG = $1; # parse energy
-  my(@strA) = split(/(?<=\.)(?=[\]\[\(\)])|(?<=[\]\[\(\)])(?=\.)/, $structA);
-  my(@strB) = split(/(?<=\.)(?=[\]\[\(\)])|(?<=[\]\[\(\)])(?=\.)/, $structB);
-  my $maxInterLenA = maxLength(\@strA, "[\[\]]");
-  my $maxInterLenB = maxLength(\@strB, "[\[\]]");
+  my(@strA) = split(/(?<=\.)(?=[\]\[\(\)])|(?<=[\]\[])(?=[\.\(\)])|(?<=[\(\)])(?=[\.\[\]])/, $structA);
+  my(@strB) = split(/(?<=\.)(?=[\]\[\(\)])|(?<=[\]\[])(?=[\.\(\)])|(?<=[\(\)])(?=[\.\[\]])/, $structB);
+  my $maxInterLenA = maxLength(\@strA, "[\\[\\]]");
+  my $maxInterLenB = maxLength(\@strB, "[\\[\\]]");
   my $maxInterLen = max($maxInterLenA, $maxInterLenB);
- 
-  return($deltaG, $structA, $structB, $maxInterLen);
+  my $amtNum = findUAinStem(\@strA, \@strB, $seqA, $seqB, "[\\[\\]]");
+  
+  return($deltaG, $structA, $structB, $maxInterLen, $amtNum);
+}
+
+# look for diagonal U's in stem given ractip
+sub findUAinStem {
+  my($structAref, $structBref, $seqA, $seqB, $char) = @_;
+  my $intSeqA = "";
+  my $revSeqB = "";
+  my @stemA;
+  my @stemB;
+  my $cnt = 0;
+  foreach my $seg (@$structAref) {
+    ($cnt += length($seg) and next) unless $seg =~ /^$char+$/;
+    $intSeqA .= substr($seqA, $cnt, length($seg));
+    push(@stemA, "." x length($seg));
+    $cnt += length($seg);
+  }
+  $cnt = 0;
+  foreach my $seg (@$structBref) {
+    ($cnt += length($seg) and next) unless $seg =~ /^$char+$/;
+    $revSeqB .= substr($seqB, $cnt, length($seg));
+    push(@stemB, "." x length($seg);
+    $cnt += length($seg);
+  }
+  $revSeqB = reverse scalar $revSeqB;
+  print "$intSeqA\n$revSeqB\n";
+  my %xlinkPos;
+  # now check for diagonal Us,  AB [[ to ]] DC
+  #                             .U    to    .U or U. to U.
+  for(my $i=0; $i < length($intSeqA) - 1; $i++) {
+    my $a = substr($intSeqA, $i, 1);
+    my $b = substr($intSeqA, $i+1, 1);
+    my $c = substr($revSeqB, $i, 1);
+    my $d = substr($revSeqB, $i+1, 1);
+    $xlinkPos{$i} = 1 if($a =~ /u|U/ and $d =~ /u|U/);
+    $xlinkPos{$i+1} = 1 if($b =~ /u|U/ and $c =~ /u|U/);
+  }
+  return(scalar(keys %xlinkPos));
 }
 
 # used by the runRactIP program.
@@ -158,9 +211,9 @@ sub maxLength {
 
 sub runBlastn {
   my($db, $basename, $threads) = @_;
-  system("blastn -query $path/../tmp/$name.fa -task blastn -db $db -word_size 20 \
+  system("blastn -query $path/../tmp/$basename.fa -task blastn -db $db -word_size 20 \
          -outfmt '6 sseqid sstart send qseqid sstrand pident length qstart qend qseq sseq evalue' \
-         -perc_identity 75 -culling_limit 1 -num_threads $threads > $path/../tmp/$name.$db.out");
+         -perc_identity 75 -culling_limit 1 -num_threads $threads > $path/../tmp/$basename.$db.out");
 }
 
 sub openBlastOutAndRemoveHits {
