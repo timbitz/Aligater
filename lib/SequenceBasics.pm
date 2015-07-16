9fc63586003e560c895c536b0652788989afb589
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2015-07-16 15:09:44 -0400

    Update SequenceBasics.pm

--- lib/SequenceBasics.pm
+++ lib/SequenceBasics.pm
@@ -16,13 +16,7 @@ require Exporter;
  
 our @ISA = qw(Exporter); 
  
-# Items to export into callers namespace by default. Note: do not export 
-# names by default without a very good reason. Use EXPORT_OK instead. 
-# Do not simply export all your public functions/methods/constants. 
- 
-# This allows declaration       use GeneRegion ':all'; 
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK 
-# will save memory. 
+
 our %EXPORT_TAGS = ( 'all' => [ qw( 
 	getSeq
         maskstr
@@ -45,17 +39,7 @@ our @EXPORT = qw(
 
 our $VERSION = '0.01'; 
 
-
-
-########################################################################################## 
-
-
-
-########################################################################################### 
-#                                                                                         # 
 #      Subroutines (EXPORTED)                                                             # 
-#                                                                                         # 
-########################################################################################### 
 
 sub revComp {
   my $seq = shift;
