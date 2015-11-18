642ab2de8aea5da557b31483f987f292e3ae7f77
tim <tim.sterne.weiler@utoronto.ca>
2015-11-18 16:17:16 -0500

    finished intramol, still debugging

--- bin/intramol.jl
+++ bin/intramol.jl
@@ -12,7 +12,10 @@ function parse_cmd()
   @add_arg_table s begin
     "--bed"
       help = "toggle output as bed format (default on)"
-      arg_type = :store_true
+      action = :store_true
+    "--biot"
+      help = "only output this specific biotype (default off)"
+      arg_type = ASCIIString
   end
   return parse_args(s)
 end
@@ -21,20 +24,37 @@ end
 ###################################################################
 function splitgb( coord )
   spl = split( coord, ':' )
-  @assert( length(spl) > 1 )
+  @assert( length(spl) > 1, "$coord does not split : into two!" )
   nums = map( x->parse(Int, x), split( spl[2], '-' ) )
   length(spl) >= 3 ? (spl[1], nums..., spl[3]) : (spl[1], nums...)
 end
 
-function read_lig_parse_reg( io )
+function read_lig_parse_reg( io; biot=nothing )
    const geneind = 25
+   const biotind = 24
+   const seqind = 11
    const posind = 17
    for l::ASCIIString in eachline( io )
-      s = split(l, '\t')
-      (g1,g2) = split( s[geneind], ':' )
+      l[1] == 'S' || continue
+      s = split(strip(l), '\t')
+      g1,g2 = split( s[geneind], ':' )
+      b1,b2 = split( s[biotind], ':' )
+      #println("$g1 -- $g2, $b1 -- $b2")
       g1 == g2 || continue
-      (c1,c2) = map( x->splitgb(x), split( s[posind], ',' ) )
-      # unfinished as of 10/2015
+      (biot != nothing && (b1 != biot || b2 != biot)) && continue
+      s[posind][1:2] == "NA" && continue
+      c1,c2 = map( x->splitgb(x), split( s[posind], ',' ) )
+      ci1,ci2 = c1[2],c2[2]
+      c1[1] == c2[1] || continue
+      @assert( c1[end] == c2[end], "$(c1[end]) does not equal $(c2[end]) !!" )
+      s1,s2 = split( s[seqind], '_' )
+      if c1[end] == "+" # if positive
+         ci1 += length(s1)
+      else # negative
+         ci2 -= length(s1)
+      end
+      first,second = tuple(sort([ci1,ci2])...)
+      println("$(c1[1])\t$first\t$second\t$g1\t0\t$(c1[end])")
    end
 end
 
@@ -48,7 +68,7 @@ end
 function main()
    pargs = parse_cmd()
 
-   reg = read_lig_parse_reg( STDIN )
+   reg = read_lig_parse_reg( STDIN, biot=pargs["biot"] )
    if pargs["bed"]
       print_bed( STDOUT, reg )
    end
